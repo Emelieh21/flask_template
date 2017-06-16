440c286f10a30845c675d8266b19919cab2916f4
Emelie Hofland <emelie.hofland@flixbus.com>
2017-06-16 18:33:52 +0200

    added some stuff

--- first_script.py
+++ first_script.py
@@ -6,18 +6,44 @@ from flask_wtf import Form
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+import os
+from flask_script import Manager
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "super secret string"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+manager = Manager(app)
 
+# classes
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+db.create_all()
+db.session.commit()
+		
 class NameForm(Form):
 	name = StringField("Hoe is jouw naam?", validators=[Required()])
 	sumbit = SubmitField("Submit")
@@ -53,6 +79,9 @@ def redirection():
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html"), 404
+	
+#if __name__ == '__main__':
+#    manager.run()	
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True)	
